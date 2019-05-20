28990919c66b7718cd963f0d5fcb3950b2f76c05
Guru <grdvnl@gmail.com>
2019-05-20 14:31:29 -0700

    bump version number

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 # Always prefer setuptools over distutils
 from setuptools import setup
 # To use a consistent encoding
@@ -14,15 +16,25 @@ from os import path
 # twine upload dist/*
 
 here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+
+def get_long_description():
+    return '''
+The table-compositor library provides the API to render data stored in table-like data structures. Currently the library supports rendering data available in a Panda’s DataFrames. The DataFrame layout is used as the table layout(including single and multi hierarchical columns/indices) by the library. The table layout is rendered directly on to an XLSX sheet or to a HTML page. Styling and layout attributes can be used to render colorful XLSX or HTML reports. The library also supports rendering of multiple dataframes into a single XLSX sheet or HTML page. The objective of the library is to be able to use the DataFrame as the API to configure the style and layout properties of the report. Callback functions are provided to customize all styling properties. The nice thing about the callback functions are that the style properties are set on cells indexed with index/column values available in the original dataframe used during rendering.
+
+Code: https://github.com/InvestmentSystems/table-compositor
+
+Docs: http://table-compositor.readthedocs.io
+
+Packages: https://pypi.python.org/pypi/table-compositor
+
+'''
 
 setup(
     name='table-compositor',
-    version='1.1.0',
+    version='1.1.2',
 
     description='Library to render table-like data structure into XLSX and other formats.',
-    long_description=long_description,
+    long_description=get_long_description(),
 
     install_requires=['pandas', 'openpyxl'],
 
